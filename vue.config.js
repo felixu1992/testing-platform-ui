5ba93cfafc193d0e12fea3d75911e59270283f33
felixu <xufei_0320@163.com>
2021-02-02 20:12:10 +0800

    :recycle: change proxy port

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
         proxy: {
             '/testing-platform-api/': {
                 // target: 'http://platform.felixu.top:7600',
-                target: 'http://localhost:8000',
+                target: 'http://localhost:20000',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/testing-platform-api/': '/'
